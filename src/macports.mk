2c3b4db4e8b8aae509ca7c7c588e5f202832d617
lymacasm <lyden.m.smith@gmail.com>
2020-08-06 23:49:19 -0700

    Adding comments/cleaning up makefiles

--- src/macports.mk
+++ src/macports.mk
@@ -1,4 +1,10 @@
+# Compiler
 CC= g++-mp-8
+
+# Compiler flags
 CFLAGS= -std=c++17 -isysroot`xcrun --show-sdk-path`
+
+# Command for printing the system time
 TIME=date
+
 export
--- src/mingw.mk
+++ src/mingw.mk
@@ -1,4 +1,10 @@
+# Compiler
 CC= g++
+
+# Compiler flags
 CFLAGS= -std=c++11
+
+# Command for printing the system time
 TIME=date
+
 export
